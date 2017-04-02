5ac45ae5a4eaf489b0196d60824d4653c03f59ac
Yunusemre Şentürk <se.yunusemre@gmail.com>
2017-04-02 13:10:37 +0300

    Change minSdkVersion 18 --> 16

--- station/camera.py
+++ station/camera.py
@@ -1,5 +1,8 @@
 import socket
 import sys
+import cv2
+import numpy as np
+
 
 def main():
     host = sys.argv[1]
@@ -10,35 +13,53 @@ def main():
 
     mySocket.listen(1)
     conn, addr = mySocket.accept()
-    print ("Connection from: " + str(addr))
-    i = 0;
+    print("Connection from: " + str(addr))
+    i = 0
     while True:
         i += 1
-        buff = ''
+        buff = b''
         while True:
             c = conn.recv(1)
-            if c == '[':
+            if c == b'[':
                 continue
-            elif c == ']':
+            elif c == b']':
                 break
             else:
                 buff += c
 
-        header = buff.split(';')
+        decoded_buff = buff.decode()
+        header = decoded_buff.split(';')
         speed_cmd = int(header[0])
         steering_cmd = int(header[1])
         speed = float(header[2])
         steering = float(header[3])
         size = int(header[4])
 
-
-        print(i, speed_cmd, steering_cmd, speed, steering, size)
+        try:
+            print(i, speed_cmd, steering_cmd, speed, steering, size)
+        except KeyboardInterrupt:
+            conn.close()
+        in_bytes = b''
         with open('/tmp/image' + str(i) + '.jpeg', 'wb') as f:
             while size > 0:
                 data = conn.recv(size)
+                in_bytes += data
                 f.write(data)
                 size -= len(data)
-    conn.close()
+
+        np_arr = np.fromstring(in_bytes, np.uint8)
+        frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+        cv2.imshow('video', frame)
+        height, width, channels = frame.shape
+        four_cc = cv2.VideoWriter_fourcc(*'XVID')
+        out = cv2.VideoWriter('out_video.avi', four_cc, 20, (width, height))
+
+        out.write(frame)
+        if (cv2.waitKey(1) & 0xFF) == ord('q'):  # Hit `q` to exit
+            break
+    out.release()
+    cv2.destroyAllWindows()
+    sys.exit(0)
 
 if __name__ == '__main__':
     main()
